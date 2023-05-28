60da49a51fb0592665bd133a641db785059ec5dc
Vener Fruet da Silveira <105865020+venerfruet@users.noreply.github.com>
2023-05-28 08:54:13 -0300

    Add files via upload

---
+++ Modulo/module-info.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * @author Vener Fruet da Silveira
+ * @version 2023-05-16
+ */
+module CSVImport {
+	
+	requires javafx.controls;
+	requires javafx.media;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires java.desktop;
+	requires java.sql;
+	
+	opens [SEU PACOTE] to javafx.controls,javafx.media,javafx.fxml,javafx.graphics;
+	
+}
