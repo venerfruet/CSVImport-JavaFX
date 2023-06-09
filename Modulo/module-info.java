a0d259e7046833fa7176dcec88a98d494fcfd9d5
Vener Fruet da Silveira <105865020+venerfruet@users.noreply.github.com>
2023-06-09 08:19:12 -0300

    Add files via upload

--- Modulo/module-info.java
+++ Modulo/module-info.java
@@ -6,14 +6,14 @@
  * @version 2023-05-16
  */
 module CSVImport {
-	
+
 	requires javafx.controls;
 	requires javafx.media;
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires java.desktop;
 	requires java.sql;
-	
-	opens [SEU PACOTE] to javafx.controls,javafx.media,javafx.fxml,javafx.graphics;
-	
+
+	opens br.com.vener.javafx.csvimport to javafx.controls, javafx.media, javafx.fxml, javafx.graphics;
+
 }
